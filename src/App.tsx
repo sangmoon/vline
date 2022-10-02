4505cc6b9d656afdc8ec0866e3900383c63f7a55
moon.park <moon.park@sendbird.com>
2022-10-02 23:59:03 +0900

    change message

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ export default function App() {
           sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
         >
           <Typography component="h1" variant="h4" align="center">
-            Vlind
+            Blind your Vine
           </Typography>
           <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
             {steps.map((label) => (
